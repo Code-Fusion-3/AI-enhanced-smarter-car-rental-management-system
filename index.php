7fd08aa84b8415cc1dc6f8b4c34a135cb6685149
MMove <niwashikilvan@gmail.com>
2025-06-12 15:19:51 +0200

    feat: Add dotenv support for environment variable management and update .gitignore

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+require_once __DIR__ . '/vendor/autoload.php';
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 session_start();
 
 require_once 'config/database.php';
